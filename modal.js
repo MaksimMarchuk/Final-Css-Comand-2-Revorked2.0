8a39a4c12c81767414cd1d798f0b460da7297e88
Maksim_Marchuk <makstop280409@gmail.com>
2026-04-16 23:40:07 +0300

    Add ice cream modal with backdrop and styling enhancements

--- modal.js
+++ modal.js
@@ -13,3 +13,19 @@
         document.body.classList.toggle("no-scroll");
     }
 })();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-2]"),
+        closeModalBtn: document.querySelector("[data-modal-close-2]"),
+        modal: document.querySelector("[data-modal-2]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
