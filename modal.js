afd837c6e9d3ee3d568485bee7050849424b57f2
Tealover2222 <pkateryna2011@gmail.com>
2026-04-19 14:08:14 +0300

    add modal milkshake

--- modal.js
+++ modal.js
@@ -61,3 +61,19 @@
         document.body.classList.toggle("no-scroll");
     }
 })();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-5]"),
+        closeModalBtn: document.querySelector("[data-modal-close-5]"),
+        modal: document.querySelector("[data-modal-5]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
