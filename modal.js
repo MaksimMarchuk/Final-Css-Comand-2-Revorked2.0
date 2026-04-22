73aa1f646fe5486060140cf7ddcad4dad80a2d55
Tealover2222 <pkateryna2011@gmail.com>
2026-04-22 21:56:23 +0300

    да-2

--- modal.js
+++ modal.js
@@ -93,3 +93,19 @@
         document.body.classList.toggle("no-scroll");
     }
 })();
+
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open-7]"),
+        closeModalBtn: document.querySelector("[data-modal-close-7]"),
+        modal: document.querySelector("[data-modal-7]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
